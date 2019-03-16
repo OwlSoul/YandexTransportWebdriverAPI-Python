c4bb7615962278c4dc6ad9b0ccf9cdaaea6d66fd
Yury D <SoulGate@yandex.ru>
2019-03-16 21:22:21 +0300

    Logger module added to the project.

---
+++ Logger.py
@@ -0,0 +1,106 @@
+"""
+This is a logger module, to simplify logging to the screen.
+Logs messages are printed in the following format:
+
+   LOG LEVEL : [ Timestamp ] : Message
+
+"""
+# pylint: disable=C0103
+#  Disabling pylint snakeCase check for module name.
+# pylint: enable=C0103
+
+
+import sys
+import datetime
+
+class Logger:
+    """
+    Logger class, handles printing log messages based on set verbosity level.
+    """
+    # Logging levels
+    NONE = 0
+    ERROR = 1
+    WARNING = 2
+    INFO = 3
+    DEBUG = 4
+
+    _verbose = ERROR
+    @property
+    def verbose(self):
+        """Current verbosity level of the program"""
+        return self._verbose
+
+    @verbose.setter
+    def verbose(self, value):
+        """Set current verbosity level of the program
+        :param value: desired verbosity level: NONE, ERROR, WARNING, INFO, DEBUG
+        :return: nothing
+        """
+        if value > self.DEBUG:
+            value = self.DEBUG
+        if value < self.NONE:
+            value = self.NONE
+        self._verbose = value
+
+    def __init__(self, log_level):
+        self.verbose = log_level
+
+    def log(self, log_level, text):
+        """
+        Log data to stderr
+        :param log_level: log level, see Application.LOG_SOMETHING constants.
+        :param text: text to print
+        :return: nothing
+        """
+        timestamp = '['+str(datetime.datetime.now())+']'
+        if log_level == self.ERROR:
+            if self.verbose >= self.ERROR:
+                print("ERROR :", timestamp, ":", str(text), file=sys.stderr)
+                return
+
+        if log_level == self.WARNING:
+            if self.verbose >= self.WARNING:
+                print("WARN  :", timestamp, ":", str(text), file=sys.stderr)
+                return
+
+        if log_level == self.INFO:
+            if self.verbose >= self.INFO:
+                print("INFO  :", timestamp, ":", str(text), file=sys.stderr)
+                return
+
+        if log_level == self.DEBUG:
+            if self.verbose >= self.DEBUG:
+                print("DEBUG :", timestamp, ":", str(text), file=sys.stderr)
+                return
+
+    def error(self, text):
+        """
+        Log error message
+        :param text: text to print
+        :return: nothing
+        """
+        self.log(self.ERROR, text)
+
+    def warning(self, text):
+        """
+                Log warning message
+                :param text: text to print
+                :return: nothing
+                """
+        self.log(self.WARNING, text)
+
+    def info(self, text):
+        """
+                Log info message
+                :param text: text to print
+                :return: nothing
+                """
+        self.log(self.INFO, text)
+
+    def debug(self, text):
+        """
+                Log debug message
+                :param text: text to print
+                :return: nothing
+                """
+        self.log(self.DEBUG, text)
--- YandexTransportWebdriverAPI.py
+++ YandexTransportWebdriverAPI.py
@@ -1,24 +1,95 @@
+#!/usr/bin/env python3
+
+# TODO: ID must be sent, not obtained from server.
+
 import socket
+import json
+
 
 class YandexTransportProxy:
     def __init__(self, host, port):
         self.host = host
         self.port = port
 
+        self.buffer_size = 4096
+
+    def _single_query_blocking(self, sock, command):
+        result = ''
+
+        command = command + '\n'
+        sock.sendall(bytes(command, 'utf-8'))
+        completed = False
+        buffer = ''
+        while not completed:
+            data = sock.recv(self.buffer_size)
+            response = bytes(data).decode('utf-8')
+            for c in response:
+                if c == '\0':
+                    json_data = json.loads(buffer)
+                    buffer = ''
+                    # Check if request was added to queue
+                    if 'queue_posistion' in json_data:
+                        if 'error' in json_data:
+                            if json_data['error'] != 'OK':
+                                raise Exception('Server error: ' + json_data['error'])
+                    # Check if expect_more_data is present and is false
+                    if 'expect_more_data' in json_data:
+                        if json_data['expect_more_data'] == False:
+                            completed = True
+
+                        if 'data' in json_data:
+                            result = json_data["data"]
+
+                else:
+                    buffer += c
+
+        return result
+
     def connect(self):
-        pass
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.host, self.port))
+        return sock
 
     def disconnect(self):
         pass
 
-    def getStopInfo(self, url):
+    def echo(self, text, block=True, callback=None):
+        """
+        Test command, will echo back the text. Note, the "echo" query is added to the Query Queue of the
+        YandexTransportProxy server, and will be executed only then it is its turn.
+        :param text: any string, for-example "Testing"
+        :param block: default true, will block until the final response will be received.
+                      Note: this may take a while, several seconds and more.
+        :param callback: Callback function to call when a new JSON is received.
+                         Used if block is set to False.
+        :return: text string, should be equal to text parameter.
+        """
+        sock = self.connect()
+
+        if sock is None:
+            raise Exception("Failed to connect to server,"
+                            " host = "+str(self.host) + "," +
+                            " post = "+str(self.port))
+
+        command = "echo?" + text
+        result = self._single_query_blocking(sock, command)
+
+        self.disconnect()
+        return result
+
+    def getStopInfo(self, url, block=True, callback=None):
         pass
 
-    def getRouteInfo(self, url):
+    def getRouteInfo(self, url, block=True, callback=None):
         pass
 
-    def getVehiclesInfo(self, url):
+    def getVehiclesInfo(self, url, block=True, callback=None):
         pass
 
-    def getAllInfo(self, url):
+    def getAllInfo(self, url, block=True, callback=None):
         pass
+
+print("Started")
+transport_proxy = YandexTransportProxy('127.0.0.1', 25555)
+result = transport_proxy.echo("msg=Hello!")
+print(json.dumps(result, sort_keys=True, indent=4, separators=(',', ': ')))
