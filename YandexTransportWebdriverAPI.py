a192a2a61138d6b922b26bb2c8a335ce9f6d5045
Yury D <SoulGate@yandex.ru>
2019-03-16 20:13:09 +0300

    Initial commit

---
+++ YandexTransportWebdriverAPI.py
@@ -0,0 +1,24 @@
+import socket
+
+class YandexTransportProxy:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def connect(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def getStopInfo(self, url):
+        pass
+
+    def getRouteInfo(self, url):
+        pass
+
+    def getVehiclesInfo(self, url):
+        pass
+
+    def getAllInfo(self, url):
+        pass
