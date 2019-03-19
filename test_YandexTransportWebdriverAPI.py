c1289d93fe20c2ed2067c199a82da919e7c4f3e1
Yury D <SoulGate@yandex.ru>
2019-03-20 02:37:47 +0300

    Unit tests added. YandexTransportWebdriverAPI functions covered:   - getEcho   - countVehiclesInfo

--- test_YandexTransportWebdriverAPI.py
+++ test_YandexTransportWebdriverAPI.py
@@ -1,7 +1,11 @@
 """
-Yandex Transp
-"""
+Yandex Transport Webdriver API - Python tests.
 
+NOTE: These are Unit Tests, they should test function behaviour based on input data only, and should NOT
+      rely on current state of Yandex API. These tests are executed once during "build" stage.
+      Do not use Live Data from Yandex MassTransit here, only saved one. Live Data is tested in
+      Integration Tests/Continuous Monitoring tests.
+"""
 
 import pytest
 import socket
