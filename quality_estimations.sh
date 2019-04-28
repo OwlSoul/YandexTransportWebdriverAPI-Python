7f13f4a9cddeb801b8948e0d4b0b522595f476b3
Yury D <SoulGate@yandex.ru>
2019-04-28 18:28:05 +0300

    Quality estimations fixed.

--- quality_estimations.sh
+++ quality_estimations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-QUALITY=$(pylint --rcfile=pylint.rc transport_proxy.py yandex_transport_core/*.py | grep -oP '(?<=Your code has been rated at).*?(?=/)')
+QUALITY=$(pylint --rcfile=pylint.rc yandex_transport_webdriver_api/*.py | grep -oP '(?<=Your code has been rated at).*?(?=/)')
 
 echo "Quality               : $QUALITY"
 echo '"Code quality"' > code_quality.csv
